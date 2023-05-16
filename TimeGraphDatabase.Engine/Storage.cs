6780b3d42db2d2104bf3b35dce9b763b1d3bf8b9
davidbetteridge <david.betteridge@zeromission.io>
2023-05-16 23:45:33 +0100

    Delete rows

--- TimeGraphDatabase.Engine/Storage.cs
+++ TimeGraphDatabase.Engine/Storage.cs
@@ -1,65 +1,138 @@
+using System.Collections;
+
 namespace TimeGraphDatabase.Engine;
 
-public class Storage
+public class Storage : IDisposable
 {
-    private const int BytesPerRow = 8 * 4;  //ulongs or uints?
+    private readonly FileStream _file;
+    private int _numberOfRows;
+    private const int BytesPerRow = 8 * 4;  //TODO:  ulongs or uints?
     public int FillFactor { get; init; } = 10;
+    private byte[] _buffer = new byte[BytesPerRow];
+    public Storage()
+    {
+        if (File.Exists(BackingFilePath()))
+        {
+            var length = new FileInfo(BackingFilePath()).Length;
+            _numberOfRows = (int)(length / BytesPerRow);
+        }
+        
+        _file = File.Open(BackingFilePath(), FileMode.OpenOrCreate);
+    }
     
     public async ValueTask InsertRowAsync(StorageRecord record)
     {
-        await using var file = File.Open(BackingFilePath(), FileMode.OpenOrCreate);  //TODO:  move up into class var
-        
-        
         // Before we can insert our row, we need to check if a filler row is required.
         // This is the case where we contain at least FillFactor rows,  and none of the
         // last FillFactor rows are fillers.
         // TODO: Keep track of last fill factor
-        var length = new FileInfo(BackingFilePath()).Length;
-        var numberOfRows = (int)(length / BytesPerRow);
         var fillerNeeded = false;
 
-        if (numberOfRows >= FillFactor)
+        if (_numberOfRows >= FillFactor)
         {
-            var buffer = new byte[BytesPerRow];  //TODO:  move up into class var
+
             fillerNeeded = true;
-            file.Seek(-FillFactor * BytesPerRow, SeekOrigin.End);
+            _file.Seek(-FillFactor * BytesPerRow, SeekOrigin.End);
             for (var lookBack = 1; lookBack <= FillFactor; lookBack++)
             {
-                file.Read(buffer, 0, BytesPerRow);
-                if (buffer[0] == 0x0 && 
-                    buffer[1] == 0x0 &&
-                    buffer[2] == 0x0 &&
-                    buffer[3] == 0x0 &&
-                    buffer[4] == 0x0 &&
-                    buffer[5] == 0x0 &&
-                    buffer[6] == 0x0 &&
-                    buffer[7] == 0x0)
+                _file.Read(_buffer, 0, BytesPerRow);
+                if (_buffer[0] == 0x0 &&
+                    _buffer[1] == 0x0 &&
+                    _buffer[2] == 0x0 &&
+                    _buffer[3] == 0x0 &&
+                    _buffer[4] == 0x0 &&
+                    _buffer[5] == 0x0 &&
+                    _buffer[6] == 0x0 &&
+                    _buffer[7] == 0x0)
                 {
                     fillerNeeded = false;
                     break;
                 }
             }
         }
-        
-        file.Seek(0, SeekOrigin.End);
+
         if (fillerNeeded)
         {
             // Write the filler
-            await file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
-            await file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
-            await file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
-            await file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));  
+            _file.Seek(0, SeekOrigin.End);
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            _numberOfRows++;
         }
+  
+        _file.Seek(0, SeekOrigin.End);
+        await _file.WriteAsync(BitConverter.GetBytes(record.Timestamp).AsMemory(0, 8));
+        await _file.WriteAsync(BitConverter.GetBytes(record.LhsId).AsMemory(0, 8));
+        await _file.WriteAsync(BitConverter.GetBytes(record.RhsId).AsMemory(0, 8));
+        await _file.WriteAsync(BitConverter.GetBytes(record.RelationshipId).AsMemory(0, 8));
+        _numberOfRows++;
+    }
+    
+    public async Task DeleteRowAsync(StorageRecord storageRecord)
+    {
+        var toDelete = BitConverter.GetBytes(storageRecord.Timestamp)
+            .Concat(BitConverter.GetBytes(storageRecord.LhsId))
+            .Concat(BitConverter.GetBytes(storageRecord.RhsId))
+            .Concat(BitConverter.GetBytes(storageRecord.RelationshipId)).ToArray();
         
-        await file.WriteAsync(BitConverter.GetBytes(record.Timestamp).AsMemory(0, 8));
-        await file.WriteAsync(BitConverter.GetBytes(record.LhsId).AsMemory(0, 8));
-        await file.WriteAsync(BitConverter.GetBytes(record.RhsId).AsMemory(0, 8));
-        await file.WriteAsync(BitConverter.GetBytes(record.RelationshipId).AsMemory(0, 8));
+        // Find the row to delete using a binary search
+        var L = 0;
+        var R = _numberOfRows - 1;
+        while (L != R)
+        {
+            var m = (int)Math.Ceiling((L + R) / 2.0);
+            _file.Seek(m * BytesPerRow, SeekOrigin.Begin);
+            _file.Read(_buffer, 0, BytesPerRow);
+
+            if (_buffer.Compare(toDelete) > 0)
+            {
+                R = m - 1;
+            }
+            else
+            {
+                L = m;
+            }
+        }
+
+        _file.Seek(L * BytesPerRow, SeekOrigin.Begin);
+        _file.Read(_buffer, 0, BytesPerRow);
+        if (_buffer.Compare(toDelete) == 0)
+        {
+            // Found
+            _file.Seek(L * BytesPerRow, SeekOrigin.Begin);
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+            await _file.WriteAsync(BitConverter.GetBytes(0L).AsMemory(0, 8));
+        }
+        else
+        {
+            // Not found
+        }
     }
 
-    public string BackingFilePath()
+    public static string BackingFilePath()
     {
         return "database.graph";
     }
 
+
+    public void Dispose()
+    {
+        _file.Dispose();
+    }
+}
+
+static class ArrayExtensions {
+    public static int Compare(this byte[] b1, byte[] b2) {
+        if (b1 == null && b2 == null)
+            return 0;
+        else if (b1 == null)
+            return -1;
+        else if (b2 == null)
+            return 1;
+        return ((IStructuralComparable) b1).CompareTo(b2, Comparer<byte>.Default);
+    }
 }
--- TimeGraphDatabase.Tests/StorageTests.cs
+++ TimeGraphDatabase.Tests/StorageTests.cs
@@ -5,14 +5,15 @@ namespace TimeGraphDatabase.Tests;
 
 public class StorageTests
 {
+    public StorageTests()
+    {
+        // Given no file currently exists
+        File.Delete(Storage.BackingFilePath()); 
+    }
+    
     [Fact]
     public async Task TheFirstRelationshipIsWrittenToTheFile()
     {
-        var storage = new Storage();
-        
-        // Given no file currently exists
-        File.Delete(storage.BackingFilePath());
-        
         var record = new StorageRecord
         {
             Timestamp = 10000,
@@ -20,11 +21,15 @@ public class StorageTests
             RhsId = 10002,
             RelationshipId = 10003
         };
-        await storage.InsertRowAsync(record);
+        
+        using (var storage = new Storage())
+        {
+            await storage.InsertRowAsync(record);
+        }
 
         // Entries are in the format:  Timestamp LhsId RhsId RelationshipId
         // ie.  each entry is 4 longs (4x8 bytes)
-        var actual = await File.ReadAllBytesAsync(storage.BackingFilePath());
+        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
         var expected = BitConverter.GetBytes(10000L)
                .Concat(BitConverter.GetBytes(10001L))
                .Concat(BitConverter.GetBytes(10002L))
@@ -35,10 +40,7 @@ public class StorageTests
     [Fact]
     public async Task TheMostRecentRelationshipIsAddedToTheEndOfTheFile()
     {
-        var storage = new Storage();
-
         // Given a file already exists which contains all 1s for the first row.
-        File.Delete(storage.BackingFilePath());
         var dummyRow = new byte[]
         {
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
@@ -46,7 +48,7 @@ public class StorageTests
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
         };
-        await File.WriteAllBytesAsync(storage.BackingFilePath(), dummyRow);
+        await File.WriteAllBytesAsync(Storage.BackingFilePath(), dummyRow);
         
         var record = new StorageRecord
         {
@@ -55,11 +57,15 @@ public class StorageTests
             RhsId = 10002,
             RelationshipId = 10003
         };
-        await storage.InsertRowAsync(record);
+        
+        using (var storage = new Storage())
+        {
+            await storage.InsertRowAsync(record);
+        }
 
         // Entries are in the format:  Timestamp LhsId RhsId RelationshipId
         // ie.  each entry is 4 longs (4x8 bytes)
-        var actual = await File.ReadAllBytesAsync(storage.BackingFilePath());
+        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
         var expected = 
                 // Dummy first row
                 dummyRow
@@ -75,44 +81,39 @@ public class StorageTests
     [Fact]
     public async Task BlocksOfTenRowsAreBrokenUpByFillerRows()
     {
-        var storage = new Storage
+        using (var storage = new Storage { FillFactor = 10 })
         {
-            FillFactor = 10
-        };
+            // Given the database contains 10 rows
+            for (ulong i = 1; i < 11; i++)
+            {
+                await storage.InsertRowAsync(new StorageRecord
+                {
+                    Timestamp = i,
+                    LhsId = i,
+                    RhsId = i,
+                    RelationshipId = i
+                });
+            }
 
-        // Given an empty database
-        File.Delete(storage.BackingFilePath());
-
-        // Given the database contains 10 rows
-        for (ulong i = 1; i < 11; i++)
-        {
+            // When the 11th and 12th rows are inserted
             await storage.InsertRowAsync(new StorageRecord
             {
-                Timestamp = i,
-                LhsId = i,
-                RhsId = i,
-                RelationshipId = i
-            }); 
+                Timestamp = 11,
+                LhsId = 11,
+                RhsId = 11,
+                RelationshipId = 11
+            });
+            await storage.InsertRowAsync(new StorageRecord
+            {
+                Timestamp = 12,
+                LhsId = 12,
+                RhsId = 12,
+                RelationshipId = 12
+            });
         }
-        
-        // When the 11th and 12th rows are inserted
-        await storage.InsertRowAsync(new StorageRecord
-        {
-            Timestamp = 11,
-            LhsId = 11,
-            RhsId = 11,
-            RelationshipId = 11
-        });
-        await storage.InsertRowAsync(new StorageRecord
-        {
-            Timestamp = 12,
-            LhsId = 12,
-            RhsId = 12,
-            RelationshipId = 12
-        });
-        
+
         // Then the file contains rows 1-10,  A Filler,  then Row 11, and then Row 12
-        var actual = await File.ReadAllBytesAsync(storage.BackingFilePath());
+        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
         var expected =
             AsRow(1,1,1,1)
                 .Concat(AsRow(2,2,2,2))
@@ -130,12 +131,61 @@ public class StorageTests
         actual.Should().BeEquivalentTo(expected);
     }
 
+    [Fact]
+    public async Task DeletingARowReplacesItWithAFiller()
+    {
+        using (var storage = new Storage { FillFactor = 10 })
+        {
+
+            // Given the database contains 10 rows
+            for (ulong i = 1; i < 11; i++)
+            {
+                await storage.InsertRowAsync(new StorageRecord
+                {
+                    Timestamp = i,
+                    LhsId = i,
+                    RhsId = i,
+                    RelationshipId = i
+                });
+            }
+
+            await storage.DeleteRowAsync(new StorageRecord
+            {
+                Timestamp = 3,
+                LhsId = 3,
+                RhsId = 3,
+                RelationshipId = 3
+            });
+        }
+
+        // Then the file has the 3rd row replaced a filler
+        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
+        var expected =
+            AsRow(1,1,1,1)
+                .Concat(AsRow(2,2,2,2))
+                .Concat(AsRow(0,0,0,0))   // Filler
+                .Concat(AsRow(4,4,4,4))
+                .Concat(AsRow(5,5,5,5))
+                .Concat(AsRow(6,6,6,6))
+                .Concat(AsRow(7,7,7,7))
+                .Concat(AsRow(8,8,8,8))
+                .Concat(AsRow(9,9,9,9))
+                .Concat(AsRow(10,10,10,10));
+        actual.Should().BeEquivalentTo(expected);
+        
+        // DATA DRIVEN TEST
+    }
+    
     private static IEnumerable<byte> AsRow(ulong i, ulong i1, ulong i2, ulong i3)
     {
         return BitConverter.GetBytes(i)
                 .Concat(BitConverter.GetBytes(i1))
                 .Concat(BitConverter.GetBytes(i2))
                 .Concat(BitConverter.GetBytes(i3));
+    }
+
+    public void Dispose()
+    {
     }
 }
 
