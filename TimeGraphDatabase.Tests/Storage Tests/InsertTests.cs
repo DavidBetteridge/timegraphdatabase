8b1ed5b8ac428fb3e192d5f5a0984983f5a803a0
davidbetteridge <david.betteridge@zeromission.io>
2023-05-27 07:55:58 +0100

    Refactor tests

--- TimeGraphDatabase.Tests/Storage Tests/InsertTests.cs
+++ TimeGraphDatabase.Tests/Storage Tests/InsertTests.cs
@@ -5,7 +5,9 @@ namespace TimeGraphDatabase.Tests;
 
 public class InsertTests : BaseStorageTest
 {
-        [Fact]
+    private const uint FILLER = 0;
+
+    [Fact]
     public async Task TheFirstRelationshipIsWrittenToTheFile()
     {
         var record = new StorageRecord
@@ -75,208 +77,98 @@ public class InsertTests : BaseStorageTest
     [Fact]
     public async Task BlocksOfTenRowsAreBrokenUpByFillerRows()
     {
-        using (var storage = new Storage { FillFactor = 10 })
-        {
-            // Given the database contains 10 rows
-            for (uint i = 1; i < 11; i++)
-            {
-                await storage.InsertRowAsync(new StorageRecord
-                {
-                    Timestamp = i,
-                    LhsId = i,
-                    RhsId = i,
-                    RelationshipId = i
-                });
-            }
+        await GivenAFileContaining(1,2,3,4,5,6,7,8,9,10);
+        
+        await WhenTheRecordIsInserted(11);
+        await WhenTheRecordIsInserted(12);
 
-            // When the 11th and 12th rows are inserted
-            await storage.InsertRowAsync(new StorageRecord
-            {
-                Timestamp = 11,
-                LhsId = 11,
-                RhsId = 11,
-                RelationshipId = 11
-            });
-            await storage.InsertRowAsync(new StorageRecord
-            {
-                Timestamp = 12,
-                LhsId = 12,
-                RhsId = 12,
-                RelationshipId = 12
-            });
-        }
-
-        // Then the file contains rows 1-10,  A Filler,  then Row 11, and then Row 12
-        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
-        var expected =
-            IsRow(1, 1, 1, 1)
-                .Concat(IsRow(2, 2, 2, 2))
-                .Concat(IsRow(3, 3, 3, 3))
-                .Concat(IsRow(4, 4, 4, 4))
-                .Concat(IsRow(5, 5, 5, 5))
-                .Concat(IsRow(6, 6, 6, 6))
-                .Concat(IsRow(7, 7, 7, 7))
-                .Concat(IsRow(8, 8, 8, 8))
-                .Concat(IsRow(9, 9, 9, 9))
-                .Concat(IsRow(10, 10, 10, 10))
-                .Concat(IsFiller())
-                .Concat(IsRow(11, 11, 11, 11))
-                .Concat(IsRow(12, 12, 12, 12));
-        actual.Should().BeEquivalentTo(expected);
+        await ThenTheFileMustContain(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, FILLER, 11, 12);
     }
-    
+
     [Fact]
     public async Task RowsCanBeInsertedAtAFillerLocation()
     {
-        // Given a file containing three rows:  1, FILLER, 3
-        await InsertAtEndOfTestFile(1);
-        await InsertAtEndOfTestFile(0);
-        await InsertAtEndOfTestFile(3);
+        await GivenAFileContaining(1, FILLER, 3);
 
-        // When a row at time 2 is inserted
-        using (var storage = new Storage { FillFactor = 10 })
-        {
-            await storage.InsertRowAsync(new StorageRecord
-            {
-                Timestamp = 2,
-                LhsId = 2,
-                RhsId = 2,
-                RelationshipId = 2
-            });
-        }
+        await WhenTheRecordIsInserted(2);
 
-        // Then the file contains 1, 2, 3
-        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
-        var expected =
-            IsRow(1)
-                .Concat(IsRow(2))
-                .Concat(IsRow(3));
-        actual.Should().BeEquivalentTo(expected);
+        await ThenTheFileMustContain(1, 2, 3);
     }
 
-    
+
     [Fact]
     public async Task RowWillBeInsertedAndUseNextFiller()
     {
-        // Given a file containing these rows:  1, 2, 3, 4, 5, 6, 8, FILLER, 10
-        await InsertAtEndOfTestFile(1);
-        await InsertAtEndOfTestFile(2);
-        await InsertAtEndOfTestFile(3);
-        await InsertAtEndOfTestFile(4);
-        await InsertAtEndOfTestFile(5);
-        await InsertAtEndOfTestFile(6); 
-        await InsertAtEndOfTestFile(8);
-        await InsertAtEndOfTestFile(0);  //FILLER
-        await InsertAtEndOfTestFile(10);
+        await GivenAFileContaining(1, 2, 3, 4, 5, 6, 8, FILLER, 10);
 
-        // When row 7 is inserted
-        using (var storage = new Storage { FillFactor = 10 })
-        {
-            await storage.InsertRowAsync(new StorageRecord
-            {
-                Timestamp = 7,
-                LhsId = 7,
-                RhsId = 7,
-                RelationshipId = 7
-            });
-        }
+        await WhenTheRecordIsInserted(7);
 
-        // Then the file contains 1, 2, 3, 4, 5, 6, 7, 8, 10
-        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
-        var expected =
-                        IsRow(1)
-                .Concat(IsRow(2))
-                .Concat(IsRow(3))
-                .Concat(IsRow(4))
-                .Concat(IsRow(5))
-                .Concat(IsRow(6))
-                .Concat(IsRow(7))
-                .Concat(IsRow(8))
-                .Concat(IsRow(10))
-            ;
-        actual.Should().BeEquivalentTo(expected);
+        await ThenTheFileMustContain(1, 2, 3, 4, 5, 6, 7, 8, 10);
     }
 
     [Fact]
     public async Task RowWillBeInsertedAndAddAFiller()
     {
-        // Given a file containing these rows:  1, 2, 3, 4, 5, 6, 8, 10
-        await InsertAtEndOfTestFile(1);
-        await InsertAtEndOfTestFile(2);
-        await InsertAtEndOfTestFile(3);
-        await InsertAtEndOfTestFile(4);
-        await InsertAtEndOfTestFile(5);
-        await InsertAtEndOfTestFile(6);
-        await InsertAtEndOfTestFile(8);
-        await InsertAtEndOfTestFile(10);
+        await GivenAFileContaining(1, 2, 3, 4, 5, 6, 8, 10);
 
-        // When row 7 is inserted
-        using (var storage = new Storage { FillFactor = 10 })
-        {
-            await storage.InsertRowAsync(new StorageRecord
-            {
-                Timestamp = 7,
-                LhsId = 7,
-                RhsId = 7,
-                RelationshipId = 7
-            });
-        }
+        await WhenTheRecordIsInserted(7);
 
-        // Then the file contains 1, 2, 3, 4, 5, 6, 7, 8, 10
-        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
-        var expected =
-                IsRow(1)
-                    .Concat(IsRow(2))
-                    .Concat(IsRow(3))
-                    .Concat(IsRow(4))
-                    .Concat(IsRow(5))
-                    .Concat(IsRow(6))
-                    .Concat(IsRow(7))
-                    .Concat(IsRow(8))
-                    .Concat(IsRow(10))
-            ;
-        actual.Should().BeEquivalentTo(expected);
+        await ThenTheFileMustContain(1, 2, 3, 4, 5, 6, 7, 8, 10);
     }
 
     [Fact]
     public async Task RowInsertedAtStart()
     {
-        // Given a file containing these rows:  2, 3, 4, 5, 6, 8, FILLER, 10
-        await InsertAtEndOfTestFile(2);
-        await InsertAtEndOfTestFile(3);
-        await InsertAtEndOfTestFile(4);
-        await InsertAtEndOfTestFile(5);
-        await InsertAtEndOfTestFile(6);
-        await InsertAtEndOfTestFile(8);
-        await InsertAtEndOfTestFile(0); //FILLER
-        await InsertAtEndOfTestFile(10);
+        await GivenAFileContaining(2, 3, 4, 5, 6, 8, FILLER, 10);
 
-        // When row 1 is inserted
-        using (var storage = new Storage { FillFactor = 10 })
+        await WhenTheRecordIsInserted(1);
+
+        await ThenTheFileMustContain(1, 2, 3, 4, 5, 6, 8, 10);
+    }
+
+    [Fact]
+    public async Task RowInsertedAtStartReplacingFiller()
+    {
+        await GivenAFileContaining(FILLER, 2, 3, 4, 5, 6, 8, FILLER, 10);
+
+        await WhenTheRecordIsInserted(1);
+
+        await ThenTheFileMustContain(1, 2, 3, 4, 5, 6, 8, FILLER, 10);
+    }
+
+    private static async Task GivenAFileContaining(params uint[] rows)
+    {
+        foreach (var row in rows)
         {
-            await storage.InsertRowAsync(new StorageRecord
-            {
-                Timestamp = 1,
-                LhsId = 1,
-                RhsId = 1,
-                RelationshipId = 1
-            });
+            await InsertAtEndOfTestFile(row);
         }
-
-        // Then the file contains 1, 2, 3, 4, 5, 6, 8, 10
-        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
-        var expected =
-                IsRow(1)
-                    .Concat(IsRow(2))
-                    .Concat(IsRow(3))
-                    .Concat(IsRow(4))
-                    .Concat(IsRow(5))
-                    .Concat(IsRow(6))
-                    .Concat(IsRow(8))
-                    .Concat(IsRow(10))
-            ;
-        actual.Should().BeEquivalentTo(expected);
     }
     
+    private static async Task WhenTheRecordIsInserted(uint value)
+    {
+        using var storage = new Storage { FillFactor = 10 };
+        await storage.InsertRowAsync(new StorageRecord
+        {
+            Timestamp = value,
+            LhsId = value,
+            RhsId = value,
+            RelationshipId = value
+        });
+    }
+
+    private static async Task ThenTheFileMustContain(params uint[] rows)
+    {
+        var actual = await File.ReadAllBytesAsync(Storage.BackingFilePath());
+        var expected = IsRow(rows[0]);
+        foreach (var row in rows[1..])
+        {
+            expected = expected.Concat(IsRow(row));
+        }
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+
+
     // TEST:  Duplicate
 }
